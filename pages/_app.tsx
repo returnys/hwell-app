c9f2d0819c1bb4ce63b805262d12f790cd31bf63
returnys <return.ys@gmail.com>
2023-09-20 12:40:31 +0900

    GA 적용중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,27 @@ import SEO from '@/next-seo.config';
 import '@/styles/globals.scss';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+     <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
