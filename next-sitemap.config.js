e7b894db1721f8ffb5a2353a8b2c1828462cefa5
returnys <return.ys@gmail.com>
2023-09-20 11:36:24 +0900

    사이트맵 적용

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://hwell-app-one.vercel.app/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
